b092104504747bc7d7773acdafe12143dc69a445
ypablo <yfedora@gmail.com>
2019-07-03 17:46:17 +0100

    28.4 react router part 2

--- src/index.js
+++ src/index.js
@@ -4,28 +4,15 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
-import country from './country.css'
 
-//import Navigation from './presentational/navigation.component';
-/*
-render(
-    <Provider store={store}>
-        <div>
-            <h1>Project</h1>
-            <DevTools />
-        </div>
-    </Provider>,
-    document.getElementById('root')
-);
-*/
 
 render(
     <Provider store={store}>
-    <div className="">
+    <div>
         <Router history={hashHistory}/>
         <h1>Project</h1>
             <DevTools />
-            </div>      
+    </div>      
     </Provider>,
     document.getElementById('root')
 )
