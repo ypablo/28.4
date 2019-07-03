56d65ab53df032ac61c329ad429fd425483307b5
ypablo <yfedora@gmail.com>
2019-07-03 12:01:17 +0100

    28.4 react route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
-
+//import Navigation from './presentational/navigation.component';
+/*
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <h1>Project</h1>
             <DevTools />
         </div>
     </Provider>,
     document.getElementById('root')
 );
+*/
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory}/>
+    </Provider>,
+    document.getElementById('root')
+)
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
 import DevTools from '../DevTools';
+import {getCountry} from '../actions/actions-countries'
 
 //const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const store = createStore(reducers, DevTools.instrument());
 
-
-store.subscribe(() => console.log(store.dispatch()))
+store.dispatch(getCountry());
 
 export default store;
